ce4d616401d6ee3221da987c2694918dabba0654
Erik Donath <erikdonath2006@gmail.com>
2025-10-31 17:59:46 +0100

    Rewrote Helpers

--- src/main/kotlin/de/erikd/lite2edit/BlockHelper.kt
+++ src/main/kotlin/de/erikd/lite2edit/BlockHelper.kt
@@ -2,13 +2,13 @@ package de.erikd.lite2edit
 
 import com.sk89q.worldedit.world.block.BlockState
 import com.sk89q.worldedit.world.block.BlockTypes
-import net.minecraft.nbt.NbtCompound
+import net.kyori.adventure.nbt.CompoundBinaryTag
 import org.slf4j.LoggerFactory
 
 object BlockHelper {
     private val logger = LoggerFactory.getLogger(BlockHelper::class.java)
 
-    fun parseBlockState(name: String, properties: NbtCompound?): BlockState? {
+    fun parseBlockState(name: String, properties: CompoundBinaryTag?): BlockState? {
         val blockType = BlockTypes.get(name)
         if (blockType == null) {
             logger.warn("Unknown block type: $name")
@@ -16,9 +16,9 @@ object BlockHelper {
         }
 
         var state = blockType.defaultState
-        if (properties == null || properties.isEmpty) return state
+        if (properties == null || properties.isEmpty()) return state
 
-        for (propName in properties.keys) {
+        for (propName in properties.keySet()) {
             val propValue = properties.getString(propName)
             try {
                 val props = blockType.properties
@@ -28,19 +28,14 @@ object BlockHelper {
                     val matchingValue = values.find { it.toString().equals(propValue, ignoreCase = true) }
                     if (matchingValue != null) {
                         @Suppress("UNCHECKED_CAST")
-                        state = state.with(
-                            property as com.sk89q.worldedit.registry.state.Property<Any>,
-                            matchingValue
-                        )
+                        state = state.with(property as com.sk89q.worldedit.registry.state.Property<Any>, matchingValue)
                     }
                 }
             } catch (e: Exception) {
                 logger.debug("Failed to set property $propName=$propValue: ${e.message}")
             }
         }
-
         return state
+        // TODO Add reverse mapping from BlockState to Litematica palette entry name/properties
     }
-
-    // TODO: Add reverse mapping from BlockState to Litematica palette entry (name + properties)
 }
