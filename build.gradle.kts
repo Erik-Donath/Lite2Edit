72fb7bc83829d419ff51639830d9ba6abb297e55
Erik Donath <erikdonath2006@gmail.com>
2025-11-01 13:33:23 +0100

    Fixed build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,24 @@ plugins {
     id("maven-publish")
 }
 
-version = project.property("mod_version") as String
-group = project.property("maven_group") as String
+
+val modVersion = project.findProperty("mod_version") as String? ?: "0.3"
+val minecraftVersion = project.findProperty("minecraft_version") as String? ?: "1.20.4"
+val archiveBaseName = project.findProperty("archives_base_name") as String? ?: "lite2edit"
+
+val loaderVersion = project.findProperty("loader_version") as String? ?: "0.17.3"
+val kotlinLoaderVersion = project.findProperty("kotlin_loader_version") as String? ?: "1.13.7+kotlin.2.2.21"
+val worldEditVersion = project.findProperty("worldedit_version") as String? ?: "7.3.0"
+
+version = modVersion
+group = "io.github.erik-donath"
 
 base {
-    archivesName.set(project.property("archives_base_name") as String)
+    archivesName.set(archiveBaseName)
 }
 
 java {
-    toolchain.languageVersion = JavaLanguageVersion.of(17)
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
     withSourcesJar()
 }
 
@@ -24,61 +33,50 @@ repositories {
     maven("https://maven.enginehub.org/repo")
 }
 
-val minecraftVersion = (project.findProperty("minecraft_version") as String?) ?: "1.20.4"
-val worldeditVersion = (project.findProperty("worldedit_version") as String?) ?: "7.3.0"
-
 dependencies {
-    minecraft("com.mojang:minecraft:$minecraftVersion")
+    minecraft("com.mojang:minecraft:${minecraftVersion}")
     mappings(loom.officialMojangMappings())
+    modImplementation("net.fabricmc:fabric-loader:${loaderVersion}")
 
-    modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")}")
-    modImplementation("net.fabricmc:fabric-language-kotlin:${project.property("kotlin_loader_version")}")
-    modImplementation("com.sk89q.worldedit:worldedit-core:$worldeditVersion")
+    // Kotlin support
+    modImplementation("net.fabricmc:fabric-language-kotlin:${kotlinLoaderVersion}")
 
+    // WorldEdit core API
+    modCompileOnly("com.sk89q.worldedit:worldedit-core:${worldEditVersion}")
+
+    // Runtime dependencies
     implementation("net.kyori:adventure-nbt:4.25.0")
     implementation("net.kyori:adventure-api:4.25.0")
     implementation("net.kyori:examination-api:1.3.0")
     implementation("net.kyori:examination-string:1.3.0")
-
     include("net.kyori:adventure-nbt:4.25.0")
     include("net.kyori:adventure-api:4.25.0")
     include("net.kyori:examination-api:1.3.0")
     include("net.kyori:examination-string:1.3.0")
 }
 
-tasks {
-    processResources {
-        inputs.property("version", project.version)
-        inputs.property("minecraft_version", minecraftVersion)
-        inputs.property("loader_version", project.property("loader_version"))
-        inputs.property("kotlin_loader_version", project.property("kotlin_loader_version"))
-        inputs.property("worldedit_version", worldeditVersion)
-
-        filteringCharset = "UTF-8"
-        filesMatching("fabric.mod.json") {
-            expand(
-                "version" to project.version,
-                "minecraft_version" to minecraftVersion,
-                "loader_version" to project.property("loader_version"),
-                "kotlin_loader_version" to project.property("kotlin_loader_version"),
-                "worldedit_version" to worldeditVersion
-            )
-        }
+tasks.withType<KotlinCompile> {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+        freeCompilerArgs.addAll("-Xjsr305=strict")
     }
+}
 
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        options.release.set(17)
-    }
+tasks.processResources {
+    inputs.property("version", project.version)
+    inputs.property("loader_version", loaderVersion)
+    inputs.property("minecraft_version", minecraftVersion)
+    inputs.property("kotlin_loader_version", kotlinLoaderVersion)
+    inputs.property("worldedit_version", worldEditVersion)
 
-    withType<KotlinCompile> {
-        compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
-    }
-
-    jar {
-        from("LICENSE") {
-            rename { "${it}_${project.base.archivesName}" }
-        }
+    filesMatching("fabric.mod.json") {
+        expand(
+            "version" to project.version,
+            "loader_version" to loaderVersion,
+            "minecraft_version" to minecraftVersion,
+            "kotlin_loader_version" to kotlinLoaderVersion,
+            "worldedit_version" to worldEditVersion
+        )
     }
 }
 
@@ -87,15 +85,22 @@ publishing {
         create<MavenPublication>("maven") {
             from(components["java"])
 
+            groupId = "io.github.erik-donath"
+            artifactId = "lite2edit-mc${minecraftVersion}"
+            version = modVersion
+
             pom {
                 name.set("Lite2Edit")
-                description.set("Fabric mod for editing Litematica schematics")
-                url.set("https://github.com/Erik-Donath/Lite2Edit")
+                description.set("A Fabric mod that lets WorldEdit open Litematica schematic files (.litematic / .ltc). Converts Litematica schematics into WorldEdit clipboards for easy importing and editing.")
+                url.set("https://github.com/Erik-Donath/lite2edit")
+                inceptionYear.set("2025")
 
                 licenses {
                     license {
-                        name.set("MIT")
+                        name.set("The MIT License")
                         url.set("https://opensource.org/licenses/MIT")
+                        distribution.set("repo")
+                        comments.set("A permissive license that is short and to the point.")
                     }
                 }
 
@@ -103,14 +108,42 @@ publishing {
                     developer {
                         id.set("erik-donath")
                         name.set("Erik Donath")
+                        email.set("erik.donath@gmail.com")
+                        url.set("https://github.com/Erik-Donath")
+                        roles.set(listOf("developer", "maintainer"))
+                        timezone.set("Europe/Berlin")
                     }
                 }
 
+                contributors {}
+
                 scm {
-                    url.set("https://github.com/Erik-Donath/Lite2Edit")
-                    connection.set("scm:git:git://github.com/Erik-Donath/Lite2Edit.git")
-                    developerConnection.set("scm:git:ssh://github.com/Erik-Donath/Lite2Edit.git")
+                    connection.set("scm:git:git://github.com/Erik-Donath/lite2edit.git")
+                    developerConnection.set("scm:git:ssh://github.com/Erik-Donath/lite2edit.git")
+                    url.set("https://github.com/Erik-Donath/lite2edit/tree/master")
+                    tag.set("HEAD")
                 }
+
+                issueManagement {
+                    system.set("GitHub Issues")
+                    url.set("https://github.com/Erik-Donath/lite2edit/issues")
+                }
+
+                ciManagement {
+                    system.set("GitHub Actions")
+                    url.set("https://github.com/Erik-Donath/lite2edit/actions")
+                }
+
+                properties.set(mapOf(
+                    "maven.compiler.source" to "21",
+                    "maven.compiler.target" to "21",
+                    "project.build.sourceEncoding" to "UTF-8",
+                    "minecraft.version" to minecraftVersion,
+                    "mod.loader" to "fabric",
+                    "fabric.loader.version" to loaderVersion,
+                    "fabric.kotlin.version" to kotlinLoaderVersion,
+                    "worldedit.version" to worldEditVersion
+                ))
             }
         }
     }
@@ -118,10 +151,10 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/Erik-Donath/Lite2Edit")
+            url = uri("https://maven.pkg.github.com/Erik-Donath/lite2edit")
             credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+                username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user") as String?
+                password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.token") as String?
             }
         }
     }
