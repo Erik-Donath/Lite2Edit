9b10293da42b54bc303fe34f9d5537c140764a44
C42xe2 <erikdonath2006@gmail.com>
2025-11-10 16:53:34 +0100

    Mark Lite2edit entrypoint as unused

--- src/main/kotlin/de/erikd/lite2edit/Lite2edit.kt
+++ src/main/kotlin/de/erikd/lite2edit/Lite2edit.kt
@@ -3,6 +3,7 @@ package de.erikd.lite2edit
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats
 import net.fabricmc.api.ModInitializer
 
+@Suppress("USUSED")
 class Lite2edit : ModInitializer {
     override fun onInitialize() {
         ClipboardFormats.registerClipboardFormat(LitematicaSchematicFormater())
