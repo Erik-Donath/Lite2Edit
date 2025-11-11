130c03114df9cb72628d4caa0f22d5aa75f61be3
Erik Donath <erikdonath2006@gmail.com>
2025-11-11 13:37:07 +0100

    Try Gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,4 +4,7 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
+    plugins {
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
+    }
 }
