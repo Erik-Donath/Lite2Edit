4820bce58b8aa8f3f36414ff76fd0bc0abff809e
Erik Donath <erikdonath@proton.me>
2026-01-26 18:24:20 +0100

    Removed import of non exsisting (deleted) file.

--- src/main/kotlin/de/erikd/lite2edit/ReadConverter.kt
+++ src/main/kotlin/de/erikd/lite2edit/ReadConverter.kt
@@ -1,17 +1,15 @@
 package de.erikd.lite2edit
 
 import de.erikd.lite2edit.litematica.FileHelper          // GZIP‑aware NBT loader
-import de.erikd.lite2edit.litematica.LitematicaReader   // Turns NBT → Schematic
-import de.erikd.lite2edit.worldedit.WEWriter           // Turns Schematic → Clipboard
+import de.erikd.lite2edit.litematica.LitematicaReader    // Turns NBT → Schematic
+import de.erikd.lite2edit.worldedit.WEWriter             // Turns Schematic → Clipboard
 import com.sk89q.worldedit.extent.clipboard.Clipboard
-import de.erikd.lite2edit.litematica.NbtStructureDebugger
 import java.io.InputStream
 
 object ReadConverter {
     @JvmStatic
     fun read(input: InputStream): Clipboard {
         val rootTag = FileHelper.loadLitematic(input)
-        //NbtStructureDebugger.dump(rootTag)
         val schematic = LitematicaReader.read(rootTag)
         return WEWriter().write(schematic)
     }
