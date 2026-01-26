fabf008a6791a34c7a83c6e70cde5d2546fb11c3
Erik Donath <erikdonath@proton.me>
2026-01-26 20:58:44 +0100

    Added isFormat support. Fixed #10

--- src/main/kotlin/de/erikd/lite2edit/LitemeticaWorldeditFormater.kt
+++ src/main/kotlin/de/erikd/lite2edit/LitemeticaWorldeditFormater.kt
@@ -4,14 +4,37 @@ import com.sk89q.worldedit.extent.clipboard.Clipboard
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardWriter
+import de.erikd.lite2edit.litematica.FileHelper
+import java.io.File
 import java.io.InputStream
 import java.io.OutputStream
 
 class LitematicaSchematicFormater : ClipboardFormat {
     override fun getName(): String = "Litematica"
-    override fun getAliases(): Set<String?> = setOf("litematic")
+    override fun getAliases(): Set<String> = setOf("litematica", "litematic", "ltc")
     override fun getPrimaryFileExtension(): String = "litematic"
-    override fun getFileExtensions(): Set<String?> = setOf("litematic")
+    override fun getFileExtensions(): Set<String> = setOf("litematic", "ltc")
+
+    override fun isFormat(file: File?): Boolean =
+        file?.name?.lowercase()?.let { name ->
+            getFileExtensions().any {
+                name.endsWith(".$it")
+            }
+        } == true
+
+    override fun isFormat(inputStream: InputStream?): Boolean =
+        inputStream?.let {
+            runCatching {
+                FileHelper.loadLitematic(it)
+            }
+            .getOrNull()
+            ?.let { root ->
+                root.contains("Version") &&
+                root.contains("SubVersion") &&
+                root.contains("MinecraftDataVersion") &&
+                root.contains("Metadata")
+                } == true
+        } ?: false
 
     override fun getReader(inputStream: InputStream?) = inputStream?.let { stream ->
         object : ClipboardReader {
