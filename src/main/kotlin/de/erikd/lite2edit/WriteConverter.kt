c711af8624e9a536004471cba301db50bae35b06
Erik Donath <erikdonath@proton.me>
2026-01-27 10:45:55 +0100

    #7 Added WEReader.kt which converts the given Clipboard in the intermediate Schematic strucutre.

--- src/main/kotlin/de/erikd/lite2edit/WriteConverter.kt
+++ src/main/kotlin/de/erikd/lite2edit/WriteConverter.kt
@@ -1,10 +1,13 @@
 package de.erikd.lite2edit
 
 import com.sk89q.worldedit.extent.clipboard.Clipboard
+import de.erikd.lite2edit.worldedit.WEReader
 import java.io.OutputStream
 
 object WriteConverter {
     fun write(clipboard: Clipboard, stream: OutputStream?) {
+        val schematic = WEReader.read(clipboard)
+        println(schematic)
         TODO("Not yet implemented")
     }
 }
---
+++ src/main/kotlin/de/erikd/lite2edit/worldedit/WEReader.kt
@@ -0,0 +1,46 @@
+package de.erikd.lite2edit.worldedit
+
+import com.sk89q.worldedit.extent.clipboard.Clipboard
+import com.sk89q.worldedit.math.BlockVector3
+import com.sk89q.worldedit.regions.Region
+import de.erikd.lite2edit.schematic.BlockBuilder
+import de.erikd.lite2edit.schematic.Schematic
+import de.erikd.lite2edit.schematic.SchematicBuilder
+import de.erikd.lite2edit.schematic.Vec3i
+import de.erikd.lite2edit.util.toKyori
+
+object WEReader {
+    @JvmStatic
+    fun read(clipboard: Clipboard): Schematic {
+        val builder = SchematicBuilder()
+
+        blockPositions(clipboard.region).forEach { pos ->
+            val baseBlock = clipboard.getFullBlock(pos)
+
+            builder.block(BlockBuilder()
+                .position(Vec3i.fromBlockVector3(pos))
+                .state(
+                    baseBlock.blockType.id,
+                    baseBlock.states.mapKeys { it.key.name },
+                    baseBlock.nbt?.toKyori()
+                )
+                .build()
+            )
+        }
+
+        return builder.build()
+    }
+
+    private fun blockPositions(region: Region): Sequence<BlockVector3> = sequence {
+        val min = region.minimumPoint
+        val max = region.maximumPoint
+
+        for (x in min.x()..max.x()) {
+            for (y in min.y()..max.y()) {
+                for (z in min.z()..max.z()) {
+                    yield(BlockVector3.at(x, y, z))
+                }
+            }
+        }
+    }
+}
